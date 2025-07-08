0438068f372b4599ec306968ca30deb2a959d601
Kirawii <113998219+Kirawii@users.noreply.github.com>
2025-07-08 16:20:22 +0800

    Update BlogPostCard.js

--- themes/hexo/components/BlogPostCard.js
+++ themes/hexo/components/BlogPostCard.js
@@ -7,22 +7,20 @@ import { BlogPostCardInfo } from './BlogPostCardInfo'
 const BlogPostCard = ({ index, post, showSummary, siteInfo }) => {
   const showPreview =
     siteConfig('HEXO_POST_LIST_PREVIEW', null, CONFIG) && post.blockMap
-  if (
-    post &&
-    !post.pageCoverThumbnail &&
-    siteConfig('HEXO_POST_LIST_COVER_DEFAULT', null, CONFIG)
-  ) {
-    post.pageCoverThumbnail = siteInfo?.pageCover
-  }
+
+  const pageCover = post?.pageCoverThumbnail ||
+    (siteConfig('HEXO_POST_LIST_COVER_DEFAULT', null, CONFIG) ? siteInfo?.pageCover : null)
+
   const showPageCover =
     siteConfig('HEXO_POST_LIST_COVER', null, CONFIG) &&
-    post?.pageCoverThumbnail &&
+    pageCover &&
     !showPreview
 
-  // 卡片交错布局的样式
-  const cardLayoutClass = siteConfig('HEXO_POST_LIST_IMG_CROSSOVER', null, CONFIG) && index % 2 === 1 
-    ? 'md:flex-row-reverse' 
-    : 'md:flex-row'
+  // 卡片交错布局样式
+  const cardLayoutClass =
+    siteConfig('HEXO_POST_LIST_IMG_CROSSOVER', null, CONFIG) && index % 2 === 1
+      ? 'md:flex-row-reverse'
+      : 'md:flex-row'
 
   return (
     <div
@@ -31,29 +29,24 @@ const BlogPostCard = ({ index, post, showSummary, siteInfo }) => {
       data-aos-duration='500'
       data-aos-once='false'
       data-aos-anchor-placement='top-bottom'
-      className={`${siteConfig('HEXO_POST_LIST_COVER_HOVER_ENLARGE', null, CONFIG) ? ' hover:scale-110 transition-all duration-150' : ''}`}>
-      
-      {/* 
-        这里是新的液态玻璃结构。
-        我们使用 `.dock` 类来获得一个比较适合卡片的圆角和边距。
-      */}
+      className={`${siteConfig('HEXO_POST_LIST_COVER_HOVER_ENLARGE', null, CONFIG)
+        ? ' hover:scale-110 transition-all duration-150'
+        : ''
+        }`}
+    >
+      {/* 液态玻璃结构 */}
       <div className="liquidGlass-wrapper dock">
         <div className="liquidGlass-effect"></div>
         <div className="liquidGlass-tint"></div>
         <div className="liquidGlass-shine"></div>
-        
-        {/* 内容层，我们在这里重建卡片的内部布局 */}
+
+        {/* 内容层 */}
         <div className="liquidGlass-text w-full">
-          
-          {/* 
-            这个 div 用来模仿原来卡片的 flex 布局。
-            它包含了交错布局的逻辑。
-          */}
           <div
-            id='blog-post-card-inner'
-            className={`w-full flex justify-between flex-col-reverse ${cardLayoutClass}`}>
-            
-            {/* 文字内容 - 完全保留，不做修改 */}
+            id="blog-post-card-inner"
+            className={`w-full flex justify-between flex-col-reverse ${cardLayoutClass}`}
+          >
+            {/* 文本信息组件 */}
             <BlogPostCardInfo
               index={index}
               post={post}
@@ -62,19 +55,19 @@ const BlogPostCard = ({ index, post, showSummary, siteInfo }) => {
               showSummary={showSummary}
             />
 
-            {/* 图片封面 */}
+            {/* 封面图 */}
             {showPageCover && (
-              
-              <div className='md:w-5/12 w-full overflow-hidden relative'>
-                <div className='w-full pt-[56.25%] relative'> {/* 16:9 高度 = 9/16 = 56.25% */}
-                  <Link href={post?.href} className='absolute top-0 left-0 w-full h-full block'>
+              <div className="md:w-5/12 w-full overflow-hidden relative">
+                <div className="w-full pt-[56.25%] relative"> {/* 16:9 高宽比 */}
+                  <Link href={post?.href} className="absolute top-0 left-0 w-full h-full block">
                     <LazyImage
                       priority={index === 1}
                       alt={`封面图 - ${post?.title}`}
-                      src={post?.pageCoverThumbnail}
-                      className='w-full h-full object-cover'
-                  />
-                </Link>
+                      src={pageCover}
+                      className="w-full h-full object-cover"
+                    />
+                  </Link>
+                </div>
               </div>
             )}
           </div>
